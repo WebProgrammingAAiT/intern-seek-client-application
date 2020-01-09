315b50f7f93c436358f93afcc5d9bbbd32e4f141
lensabillion <56606537+lensabillion@users.noreply.github.com>
2020-01-10 02:57:20 +0300

    Update repository.go

--- user/repository.go
+++ user/repository.go
@@ -10,3 +10,10 @@ type UserRepository interface {
 	Users() ([]entity.User, error)
 	User(id int) (entity.User, error)
 }
+type ApplicationRepository interface{
+	StoreApplication(application *entity.Application)(*entity.Application,[]error)
+	UpdateApplication(application *entity.Application)(*entity.Application,[]error)
+	DeleteApplication(id uint)(*entity.Application,[]error)
+	Applications()([]entity.Application,[]error)
+	Application(id uint)(*entity.Application,[]error)
+}
