c5f8d7e636ada1da3d58a2a0ad280e90b4da81c3
lensabillion <56606537+lensabillion@users.noreply.github.com>
2020-01-10 02:20:23 +0300

    application gorm

--- entity/application.go
+++ entity/application.go
@@ -1,8 +1,16 @@
 package entity
 
+import "github.com/jinzhu/gorm"
 // type Application struct {
 // 	work      Work
 // 	applicant Intern
 // 	date      string
 // 	status    bool
 // }
+type Application struct {
+	gorm.Model
+	internshipID    uint   `sql:"type:int REFERENCES internship(ID)"`
+	applicantID     uint   `sql:"type:int REFERENCES internship(ID)"`
+	applicationDate string `gorm:"type:timestamp;not null"`
+	status          string `gorm:"type:varchar(30);not null"`
+}
