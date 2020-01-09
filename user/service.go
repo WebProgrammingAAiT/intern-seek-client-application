7bb292c9733edd4813119b82584d62d1a997af28
lensabillion <56606537+lensabillion@users.noreply.github.com>
2020-01-10 02:58:19 +0300

    Update service.go

--- user/service.go
+++ user/service.go
@@ -10,3 +10,10 @@ type UserService interface {
 	Users() ([]entity.User, error)
 	User(id int) (entity.User, error)
 }
+type ApplicationService interface {
+	StoreApplication(application *entity.Application)(*entity.Application,[]error)
+	UpdateApplication(application *entity.Application)(*entity.Application,[]error)
+	DeleteApplication(id uint)(*entity.Application,[]error)
+	Applications()([]entity.Application,[]error)
+	Application(id uint)(*entity.Application,[]error)
+}
